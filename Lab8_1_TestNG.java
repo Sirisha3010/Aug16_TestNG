e13f9dc9bbe6d028e7c05e44fae6b3a3b3cd6add
Sirisha3010 <sirisha30102002@gmail.com>
2025-08-21 17:36:41 +0530

    done

--- Lab8_1_TestNG.java
+++ Lab8_1_TestNG.java
@@ -1,7 +1,10 @@
 package TestNG_programs;
 
 
-import org.testng.annotations.Test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -15,9 +18,6 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
